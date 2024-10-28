a172ca1177287a57de3e136135c8c4689621933c
HarshUleedri <ulidari2@gmail.com>
2024-10-28 12:21:39 +0530

    feat: :sparkles: changed config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'Vue-projects-',
+  base:'/Vue-projects-/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
